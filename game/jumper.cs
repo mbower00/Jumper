cd8791fa470a0410fd8c4d2cdf2fc395a635eede
Jesse Evans <eva20005@byui.edu>
2022-02-07 12:29:28 -0700

    Added some data to jumper.  - Jesse

--- game/jumper.cs
+++ game/jumper.cs
@@ -1,1 +1,39 @@
+// Keeps track of how much damage the chute has taken, and how much of the word they have guessed
 
+using System;
+
+namespace cse210_jumper.game{
+    public class Jumper{
+        private int chuteDamage;
+        private List<string> currentStatus = new List[];
+        
+        private bool interpretGuess(); // Compares secret word to input
+        public void updateStatus(); // Determines if input and secret code match  --  changes damage
+        public int getChuteDamage(); // return damage level
+        public list getStatus(); // contains data of the Underscores and Letters
+    }
+}
+
+/*
+Attributes:
+    chuteDamage: int
+    currentStatus: string
+Methods:
+    InterpretGuess(): bool
+    UpdateStatus(): void
+    GetChuteDamage(): int
+    GetStatus(): list
+*/
+// ------------------------------------------------------------
+/*
+Behaviors (Methods):
+    Changes underscore to correctly guessed letters
+    Send game status
+    Receives the correct/incorrect guess
+    Interprets the guess based off received secret letters
+    Updates status
+    Send chute damage to terminal service
+States (Attributes):
+    Chute damage: int
+    Status: string
+*/
--- program.cs
+++ program.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace cse210_jumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
