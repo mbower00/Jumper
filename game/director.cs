bb4c3e585e99bfe337d05d18dc2721d2c119b62b
nvporter <nvporterhouse@gmail.com>
2022-02-07 12:29:56 -0700

    testing

--- game/director.cs
+++ game/director.cs
@@ -1,1 +1,84 @@
+using Systems;
+
+//director class
+//Object Director
+//Responsibility (what it does): 
+//Receives input from user for the different aspects of the game, controls game flow
+//Behaviors (Methods):
+//Receive an answer to play again?
+//Receives input for guesses
+//Updates wins and losses
+//Start game
+//Sends guess
+//Same guess
+//States (Attributes):
+//Play again choice: string
+//Play again: bool
+//guess : string
+
+//Class Director
+//Attributes:
+//continuePlayChoice: string
+//playerGuess: string
+//gameEnding: bool
+//Methods:
+//ContinuePlay(): bool
+//NewGuess(): string
+//GameOutcome(): bool
+//StartGame(): void
+//IsSameGuess: bool
+
+namespace cse210_jumper.game{
+    public class director{
+        string gameContinue = "y";
+        string playerGuess = 
+        bool gameEnding = true;
+        
+
+
+
+
+
+
+    }
+}
+     public void StartGame()
+      {
+
+      }
+    public void NewGuess()
+    {
+        
+    }
+    public bool IsSameGuess()
+    {
+        if (playerGuess()){
+
+        }
+    }
+    public void gameContinue()
+    {
+        Console.Writeline("")
+    }  
+    44
+    
+
+    public void Continuegame()
+    {
+        string keepPlaying = Console.ReadLine(); // gets input and applys it to keepPlaying
+            isPlaying = (keepPlaying == "y");
+            Console.WriteLine(""); // print blank line
+
+    }
+    
+    public void GameOutcome()
+    {
+
+    }
+   
+   
+
+
+
+
 
