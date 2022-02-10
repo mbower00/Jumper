27226816700ea5c95e004c94c5c5583875eef3e1
nvporter <nvporterhouse@gmail.com>
2022-02-10 12:15:05 -0700

    ammended mistakes

--- game/director.cs
+++ game/director.cs
@@ -19,7 +19,7 @@ using Systems;
 //Class Director
 //Attributes:
 //continuePlayChoice: string
-//playerGuess: string
+//playerGuess: char
 //gameEnding: bool
 //Methods:
 //ContinuePlay(): bool
@@ -33,7 +33,7 @@ using Systems;
 namespace cse210_jumper.game{
     public class Director{
         
-        private string playerGuess = "0";
+        private char playerGuess = '0';
         private Jumper jumper = new Jumper();
         private bool keepPlaying = true; 
         private Puzzle puzzle = new Puzzle();
@@ -45,24 +45,23 @@ namespace cse210_jumper.game{
           while(keepPlaying){ ///here we loop the whole game itself, after the conclusion of each game it'll ask if they'd like to play again
               while(true){ /// here we loop the steps of the game with the functions we have called from the other classes and functions we have written
               GetInputs();
+              puzzle.GetGuesses(playerGuess);
               DoUpdates();
               DoOutputs();
             }
             ContinueGame();
           }
       /// <summary>
-      /// 
-      /// 
-      /// 
+      /// Constructor
       /// </summary>    
-
       }
         public Director(){
 
         }
         
-        private void GetInputs(){ /// showing the user the promts written in the terminal class and calling them here
-            terminal.DisplayPrompt()
+        public void GetInputs(){ /// showing the user the promts written in the terminal class and calling them here
+            terminal.DisplayPrompt();
+            playerGuess = Console.ReadLine();
             
 
         }
@@ -70,19 +69,19 @@ namespace cse210_jumper.game{
         /// like the ground or the blanks of letters
         {
             
-            terminal.DisplayStatus(jumper)
-            terminal.DisplayScene(jumper)
+            terminal.DisplayStatus(jumper);
+            terminal.DisplayScene(jumper);
         }
         private void DoOutputs(){ /// Now we are goning to get the information needed to call the game, if the chute is damaged 5 times they lose the game while
         /// if all the "_" are removed before that they win the game
             
-                else if (jumper.GetChuteDamage() == 5) /// call from jumper to get the damage and checking to see if the player has exceded 5 yet
+                if (jumper.GetChuteDamage() == 5) /// call from jumper to get the damage and checking to see if the player has exceded 5 yet
                 {
-                    terminal.DisplayLose() /// displays the loss function called from terminal
+                    terminal.DisplayLose(); /// displays the loss function called from terminal
                     break;
                 } 
-                if !(jumper.GetStatus().Contains("_")){ /// now we pull from jumper to see if the blanks are gone
-                    terminal.DisplayWin()  /// displays the win function called from terminal
+                else if (!jumper.GetStatus().Contains("_")){ /// now we pull from jumper to see if the blanks are gone
+                    terminal.DisplayWin();  /// displays the win function called from terminal
                     break;
                 }
             
