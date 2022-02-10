90b0c9817b8a1472515969c829ee8b50e41070ef
cammieleigh <cammie.peterson18@gmail.com>
2022-02-10 11:33:35 -0700

    changes to puzzle class

--- game/puzzle.cs
+++ game/puzzle.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace cse210_jumper.game{
+
 
     ///Holds a list of words to randomly pull words from, holds the pulled word, 
     ///and determines if the guess is correct or not
@@ -12,29 +12,63 @@ namespace cse210_jumper.game{
         {
 
         }
-
         public string WordList()
         {
 
-            private List<string> wordBank = File.ReadAllLines("words.txt").ToList();
-            string newWord = random.next(wordBank.count);
-            return newWord;
+            List<string> wordBank = new List<string>();  
+            wordBank.Add("imminent");  
+            wordBank.Add("giant"); 
+            wordBank.Add("glitter"); 
+            wordBank.Add("clothes"); 
+            wordBank.Add("soccer"); 
+            wordBank.Add("thunder"); 
+            wordBank.Add("lightning"); 
+            wordBank.Add("train"); 
+            wordBank.Add("bike");         
+            int newWord = random.Next(wordBank.Count);
+            Console.WriteLine(wordBank[newWord]);
+            return wordBank[newWord];
             
         }
 
-        public string LettersNeeded()
+        public List<char> LettersNeeded(string newWord)
         {
-            private List<string> wordLetters = new List<string>();
-            
+            List<char> wordLetters = new List<char>();
 
+            for (int i = 0; i < newWord.Length; i++)
+            {
+                wordLetters.Add(newWord[i]);
+                Console.WriteLine(wordLetters[i]);
+            }
+            
+            return wordLetters;
 
         }
 
-        public string GetGuesses()
+        public List<int> GetGuesses(List<char> wordLetters)
         {
-            public string lettersGuessed = Director.playerGuess();
+            
+            
+            public string lettersGuessed = Director.GetInputs();
+            int letNum = (int) lettersGuessed % 32;
+            List<int> correctGuess = new List<int>();
+            for (int i = 0; i < wordLetters.Count; i++ )
+             {
+                if (letNum == (int) wordLetters[i] % 32)
+                {
+                    Console.WriteLine(i);
+                    correctGuess.Add(i);
+                    Console.WriteLine(correctGuess);
+
+                }
+                 
+                else
+                {
+                    correctGuess.Add(-1);
+                }
+             }
+             return correctGuess;
 
         }
 
     }
-}
