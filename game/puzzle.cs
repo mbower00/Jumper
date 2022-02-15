d9daa94606b52985bd1a5b54b5f720f8b627c022
cammieleigh <cammie.peterson18@gmail.com>
2022-02-15 11:41:54 -0700

    new puzzle commit

--- game/puzzle.cs
+++ game/puzzle.cs
@@ -1,17 +1,21 @@
 using System;
 
-
-
     ///Holds a list of words to randomly pull words from, holds the pulled word, 
     ///and determines if the guess is correct or not
     public class Puzzle{
 
         Random random = new Random();
+        List<char> wordLetters = new List<char>();
 
         public Puzzle()
         {
 
         }
+
+        /// <summary>
+        /// Holds a list of words to be pulled from
+        /// </summary>
+        /// <returns>one word to be played with</returns>
         public string WordList()
         {
 
@@ -26,14 +30,18 @@ using System;
             wordBank.Add("train"); 
             wordBank.Add("bike");         
             int newWord = random.Next(wordBank.Count);
-            Console.WriteLine(wordBank[newWord]);
             return wordBank[newWord];
             
         }
 
-        public List<char> LettersNeeded(string newWord)
+        /// <summary>
+        /// Takes word from WordList and turns it into a list of characters
+        /// </summary>
+        /// <param name="newWord"></param>
+        /// <returns>list of characters made from the word</returns>
+        public List<char> LettersNeeded()
         {
-            List<char> wordLetters = new List<char>();
+            
 
             for (int i = 0; i < newWord.Length; i++)
             {
@@ -45,13 +53,18 @@ using System;
 
         }
 
-        public List<int> GetGuesses(List<char> wordLetters)
+        /// <summary>
+        /// Compares the player's guess to the word used to see if the letter 
+        /// they guess is in the word
+        /// </summary>
+        /// <param name="wordLetters"></param>
+        /// <returns>the index number(s) of the letter guess if correct</returns>
+        public List<int> GetGuesses(playerGuess)
         {
-            
-            
-            public string lettersGuessed = Director.GetInputs();
+            public string lettersGuessed = playerGuess;
             int letNum = (int) lettersGuessed % 32;
             List<int> correctGuess = new List<int>();
+
             for (int i = 0; i < wordLetters.Count; i++ )
              {
                 if (letNum == (int) wordLetters[i] % 32)
